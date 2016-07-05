8cb0f8ba9be39900dc50b6abc45f06ecb8df48cd
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2016-07-06 00:13:30 +0300

    Playing with login via Google

---
+++ src/main/java/edu/softserveinc/healthbody/webclient/GoogleApiDataLoader.java
@@ -0,0 +1,9 @@
+package edu.softserveinc.healthbody.webclient;
+
+public class GoogleApiDataLoader {
+	
+	public String getUserData(String token){
+		return "Not implemented yet";
+	}
+
+}
--- src/main/java/edu/softserveinc/healthbody/webclient/servlets/GoogleTestServlet.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/servlets/GoogleTestServlet.java
@@ -1,6 +1,9 @@
 package edu.softserveinc.healthbody.webclient.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,29 +16,73 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/gt")
 public class GoogleTestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GoogleTestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public GoogleTestServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter out = response.getWriter();
+		String rn = System.lineSeparator();
+		out.print("<html lang=\"en\">" + rn);
+		out.print("<head>" + rn);
+		out.print("<meta name=\"google-signin-scope\" content=\"profile email\">" + rn);
+		out.print(
+				"<meta name=\"google-signin-client_id\" content=\"48524677967-juniqolaio06efre3m3q7774097q50u8.apps.googleusercontent.com\">"
+						+ rn);
+		out.print("<script src=\"https://apis.google.com/js/platform.js\" async defer></script>" + rn);
+		out.print("</head>" + rn);
+		out.print("<body>" + rn);
+		out.print("<div class=\"g-signin2\" data-onsuccess=\"onSignIn\" data-theme=\"dark\"></div>" + rn);
+		out.print("<script>" + rn);
+		out.print("function onSignIn(googleUser) {" + rn);
+		out.print("// Useful data for your client-side scripts:" + rn);
+		out.print("var profile = googleUser.getBasicProfile();" + rn);
+		out.print("console.log(\"ID: \" + profile.getId()); // Don't send this directly to your server!" + rn);
+		out.print("console.log('Full Name: ' + profile.getName());" + rn);
+		out.print("console.log('Given Name: ' + profile.getGivenName());" + rn);
+		out.print("console.log('Family Name: ' + profile.getFamilyName());" + rn);
+		out.print("console.log(\"Image URL: \" + profile.getImageUrl());" + rn);
+		out.print("console.log(\"Email: \" + profile.getEmail());" + rn);
+		out.print("// The ID token you need to pass to your backend:" + rn);
+		out.print("var id_token = googleUser.getAuthResponse().id_token;" + rn);
+		out.print("console.log(\"ID Token: \" + id_token);" + rn);
+		out.print("console.log(\"JSON: \" + JSON.stringify(googleUser));" + rn);
+		out.print("};" + rn);
+		out.print("</script>" + rn);
+		out.print("</body>" + rn);
+		out.print("</html>" + rn);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null)
+				sb.append(line);
+		} catch (Exception e) {
+		}
+
+		System.out.println("DO POST was here");
+		System.out.println(sb.toString());
 	}
 
 }
