b6308558fe461a74535cda51c339003b7a98ab32
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2016-07-08 16:30:59 +0300

    Simple login page added. No actions yet.

--- src/main/java/edu/softserveinc/healthbody/webclient/servlets/GoogleTestServlet.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/servlets/GoogleTestServlet.java
@@ -22,7 +22,6 @@ public class GoogleTestServlet extends HttpServlet {
 	 */
 	public GoogleTestServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -31,7 +30,6 @@ public class GoogleTestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		String rn = System.lineSeparator();
 		out.print("<html lang=\"en\">" + rn);
@@ -70,8 +68,6 @@ public class GoogleTestServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// doGet(request, response);
 		StringBuffer sb = new StringBuffer();
 		String line = null;
 		try {
