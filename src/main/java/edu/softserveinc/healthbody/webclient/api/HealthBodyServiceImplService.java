52edd35402546c983edd725a0c9cabc5aa80cc73
Iryna Kret <irynakret@gmail.com>
2016-07-26 11:10:31 +0300

    Added MainController

--- src/main/java/edu/softserveinc/healthbody/webclient/api/HealthBodyServiceImplService.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/api/HealthBodyServiceImplService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "HealthBodyServiceImplService", targetNamespace = "http://webservice.healthbody.softserveinc.edu/", wsdlLocation = "http://ws-healthbody.rhcloud.com/HealthBody-WebService/HealthBodyService?wsdl")
+@WebServiceClient(name = "HealthBodyServiceImplService", targetNamespace = "http://webservice.healthbody.softserveinc.edu/", wsdlLocation = "http://localhost:8080/HealthBody-WebService/HealthBodyService?wsdl")
 public class HealthBodyServiceImplService
     extends Service
 {
@@ -30,7 +30,7 @@ public class HealthBodyServiceImplService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://ws-healthbody.rhcloud.com/HealthBody-WebService/HealthBodyService?wsdl");
+            url = new URL("http://localhost:8080/HealthBody-WebService/HealthBodyService?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
--- src/main/java/edu/softserveinc/healthbody/webclient/controllers/MainController.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/controllers/MainController.java
@@ -1,28 +1,20 @@
 package edu.softserveinc.healthbody.webclient.controllers;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.softserveinc.healthbody.webclient.api.HealthBodyService;
-import edu.softserveinc.healthbody.webclient.api.UserDTO;
+import edu.softserveinc.healthbody.webclient.api.HealthBodyServiceImplService;
 
 @Controller
 public class MainController {
 
-	@RequestMapping(value = "/userlist.html", method = RequestMethod.GET)
-	public String getBudget(@ModelAttribute("user") UserDTO user, Model model) {
-
-		List<UserDTO> userDTO = HealthBodyService.getAllUsers(1, 5);
-
-		model.addAttribute("GetAllUsers", userDTO);
-
+	@RequestMapping(value = "/userlist.html")
+	public String getBudget(Model model) {
+		HealthBodyServiceImplService healthBody = new HealthBodyServiceImplService();
+		HealthBodyService service = healthBody.getHealthBodyServiceImplPort();
+		model.addAttribute("AllUsers", service.getAllUsers(1, 5));
 		return "userlist";
-
 	}
-
 }
