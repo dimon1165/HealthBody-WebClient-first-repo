eec082880fdbdd8f75eac06cf6cfd0f0324068aa
Dmytro Voropai <dimon1165@gmail.com>
2016-08-01 20:38:36 +0300

    GroupController modified

--- src/main/java/edu/softserveinc/healthbody/webclient/controllers/GroupController.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/controllers/GroupController.java
@@ -1,7 +1,6 @@
 package edu.softserveinc.healthbody.webclient.controllers;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +10,10 @@ import edu.softserveinc.healthbody.webclient.api.HealthBodyServiceImplService;
 
 @Controller
 public class GroupController {
-	private ApplicationContext context;
 
 	@RequestMapping(value = "/group.html")
-	public String getUserList(Model model, String name) {
-		context = new AnnotationConfigApplicationContext(HealthBodyServiceImplService.class);
-		HealthBodyServiceImplService healthBody = context.getBean(HealthBodyServiceImplService.class);
+	public String getUserList(Model model, @Autowired HealthBodyServiceImplService healthBody, String name) {
+		
 		HealthBodyService service = healthBody.getHealthBodyServiceImplPort();
 		model.addAttribute("getGroup", service.getGroupByName(name));
 		return "group";
