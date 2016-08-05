9c4c5610075d2e66fc9ebbcc1c0c361ea10e4212
Pavlo Fedoryshyn <fedorushunp@gmail.com>
2016-08-05 12:58:43 +0300

    user cabinet for loged in user

--- src/main/java/edu/softserveinc/healthbody/webclient/controllers/UserCabinetController.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/controllers/UserCabinetController.java
@@ -1,9 +1,12 @@
 package edu.softserveinc.healthbody.webclient.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.softserveinc.healthbody.webclient.api.HealthBodyService;
 import edu.softserveinc.healthbody.webclient.api.HealthBodyServiceImplService;
@@ -11,12 +14,12 @@ import edu.softserveinc.healthbody.webclient.api.HealthBodyServiceImplService;
 @Controller
 public class UserCabinetController {
 
-	@RequestMapping(value = "/usercabinet.html")
-	public String getUserList(Model model, @Autowired HealthBodyServiceImplService healthBody, String userLogin) {
-
+	@RequestMapping(value = "/usercabinet.html",method = RequestMethod.GET)
+	public String getUserList(Model model, @Autowired HealthBodyServiceImplService healthBody,HttpServletRequest request) {
+		String userLogin = request.getUserPrincipal().getName();
 		HealthBodyService service = healthBody.getHealthBodyServiceImplPort();
 		model.addAttribute("getUser", service.getUserByLogin(userLogin));
 		return "usercabinet";
 	}
-	
+
 }
