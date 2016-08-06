aa7b4a18bde3c55fab44afd29df167b49dbe277f
Pavlo Fedoryshyn <fedorushunp@gmail.com>
2016-08-06 13:14:13 +0300

    package fix

--- src/main/java/edu/softserveinc/healthbody/webclient/config/SecurityConfig.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package edu.softserve.healthbody.webclient.config;
+package edu.softserveinc.healthbody.webclient.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
